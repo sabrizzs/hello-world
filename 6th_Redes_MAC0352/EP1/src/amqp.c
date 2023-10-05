a71b01d34eae437f85c6054a5d14001d89f38d97
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-05 17:31:10 -0300

    Update amqp.c

--- 6th_Redes_MAC0352/EP1/src/amqp.c
+++ 6th_Redes_MAC0352/EP1/src/amqp.c
@@ -240,11 +240,11 @@ void* mallocSharedData(size_t size){
 
 void initializeQueuesData(){
 
-    queues_data.queues = mallocSharedData(MAXQUEUESIZE * sizeof(char*));
-    queues_data.queues->name = mallocSharedData(MAXQUEUENAMESIZE * sizeof(char*));
-    queues_data.queues->messages = mallocSharedData(MAXMESSAGENUMBER * sizeof(char*));
-    queues_data.queues->messages->data = mallocSharedData(MAXMESSAGESIZE * sizeof(char*));
-    queues_data.queues->messages->consumers = mallocSharedData(MAXCONSUMERNUMBER * sizeof(char*));
+    queues_data.queues = (char**)mallocSharedData(MAXQUEUESIZE * sizeof(char*));
+    queues_data.queues->name = (char**)mallocSharedData(MAXQUEUENAMESIZE * sizeof(char*));
+    queues_data.queues->messages = (char**)mallocSharedData(MAXMESSAGENUMBER * sizeof(char*));
+    queues_data.queues->messages->data = (char**)mallocSharedData(MAXMESSAGESIZE * sizeof(char*));
+    queues_data.queues->messages->consumers = (char**)mallocSharedData(MAXCONSUMERNUMBER * sizeof(char*));
 
     for (int i = 0; i < MAXQUEUESIZE; i++) {
         strcpy(queues_data.queues[i].name, "");
