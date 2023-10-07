0a460bf691add5e78c6495b7b9814bb0fe024190
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-07 18:22:16 -0300

    Update amqp.c

--- 6th_Redes_MAC0352/EP1/src/amqp.c
+++ 6th_Redes_MAC0352/EP1/src/amqp.c
@@ -529,11 +529,13 @@ void moveConsumer(int index){
 /* dando errado */
 void removeMessage(int index){
     // clear the message at the front of the queue
-    memcpy(queues.messages[index][0], '\0', sizeof(char));
+    //memcpy(queues.messages[index][0], '\0', sizeof(char));
+    memset(queues.messages[index][0].data, 0, MAXMESSAGESIZE);
     for(int i = 0; i < MAXMESSAGENUMBER - 1; i++) {
         if(strcmp(queues.messages[index][i + 1], "") != 0){
             memcpy(queues.messages[index][i],queues.messages[index][i + 1], strlen(queues.messages[index][i + 1]));
-            memcpy(queues.messages[index][i + 1], "", sizeof(char));
+            //memcpy(queues.messages[index][i + 1], "", sizeof(char));
+            memset(queues.messages[index][i + 1].data, 0, MAXMESSAGESIZE);
         } else {
             return;
         }
