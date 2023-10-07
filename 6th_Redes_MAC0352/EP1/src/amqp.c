0b40054d7e8c9519feae1f176248f20c74f7a846
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-07 17:42:04 -0300

    Update amqp.c

--- 6th_Redes_MAC0352/EP1/src/amqp.c
+++ 6th_Redes_MAC0352/EP1/src/amqp.c
@@ -303,8 +303,8 @@ void publishMethod(int connfd, char *recvline, u_int32_t size){
     read(connfd,recvline, length + 4);
     read(connfd,recvline, 4);//content body length
     length = ntohl(*((u_int32_t*)recvline));
-  
-    read(connfd,recvline, length + 1);
+    printf("Tamanho da mensagem: %d", length);
+    read(connfd, recvline, length + 1);
     memcpy(messageData, recvline, length); // testar com -1
     //messageData[length] = "\0";
     /* Mensagem com um caractere desconhecido no final */
