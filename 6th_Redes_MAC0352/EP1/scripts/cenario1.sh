2fd987bb1b262bafec6418bedb0040ef9c004b9a
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-08 18:37:16 -0300

    script

---
+++ 6th_Redes_MAC0352/EP1/scripts/cenario1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Inicie o servidor em um contêiner com base na imagem que você construiu
+docker run -d --name servidor -p 5672:5672 cenario1
+
+# Aguarde alguns segundos para garantir que o servidor esteja totalmente iniciado
+sleep 10
+
+# Execute suas métricas de uso de rede e CPU aqui (por exemplo, uso do 'docker stats')
+# Exemplo: Execute seus testes de servidor AMQP aqui
+
+# Coletar métricas de uso de rede e CPU do contêiner
+docker stats servidor
+
+# Pare e remova o contêiner do servidor
+docker stop servidor
+docker rm servidor
