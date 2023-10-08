6601dfb56ed2291469dddef77066061279fe9f4f
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-08 19:33:38 -0300

    tests

--- 6th_Redes_MAC0352/EP1/scripts/runTests.sh
+++ 6th_Redes_MAC0352/EP1/scripts/runTests.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Execute o script principal para 0 clientes e redirecione a saída para um arquivo
-bash scripts/tests.sh 0 > resultados_0_clientes.txt
-
-# Execute o script principal para 10 clientes e redirecione a saída para um arquivo
-bash scripts/tests.sh 10 > resultados_10_clientes.txt
-
-# Execute o script principal para 100 clientes e redirecione a saída para um arquivo
-bash scripts/tests.sh 100 > resultados_100_clientes.txt
+bash scripts/tests.sh 0
+bash scripts/tests.sh 10
+bash scripts/tests.sh 100
--- 6th_Redes_MAC0352/EP1/scripts/tests.sh
+++ 6th_Redes_MAC0352/EP1/scripts/tests.sh
@@ -46,9 +46,8 @@ done
 sleep 30
 
 # Collect network and CPU usage metrics from the server container
-echo "---------------------------------"
-echo "Metrics for the server container:"
-docker stats servidor --no-stream --format "table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}"
+output_file="results_${NUM_CLIENTES}_clients.txt"
+docker stats servidor --no-stream --format "table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}" > "$output_file"
 
 # Stop and remove the server container
 docker stop servidor
