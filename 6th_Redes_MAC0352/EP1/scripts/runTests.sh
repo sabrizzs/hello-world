9b51d5fde8d2bca39523c25dfecc8abbdb3cdc07
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-08 19:02:11 -0300

    Update runTests.sh

--- 6th_Redes_MAC0352/EP1/scripts/runTests.sh
+++ 6th_Redes_MAC0352/EP1/scripts/runTests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Execute o script principal para 0 clientes e redirecione a saída para um arquivo
-bash tests.sh 0 > resultados_0_clientes.txt
+bash scripts/tests.sh 0 > resultados_0_clientes.txt
 
 # Execute o script principal para 10 clientes e redirecione a saída para um arquivo
-bash tests.sh 10 > resultados_10_clientes.txt
+bash scripts/tests.sh 10 > resultados_10_clientes.txt
 
 # Execute o script principal para 100 clientes e redirecione a saída para um arquivo
-bash tests.sh 100 > resultados_100_clientes.txt
+bash scripts/tests.sh 100 > resultados_100_clientes.txt
