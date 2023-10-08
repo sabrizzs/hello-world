4c9c651d51c11207716b193d229c9c352f255ef3
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-08 19:22:06 -0300

    Update tests.sh

--- 6th_Redes_MAC0352/EP1/scripts/tests.sh
+++ 6th_Redes_MAC0352/EP1/scripts/tests.sh
@@ -1,51 +1,53 @@
 #!/bin/bash
 
-# Verifica o número correto de argumentos
+# Check the correct number of arguments
 if [[ $# -ne 1 ]]; then
-  echo "Uso incorreto! Forneça o número de clientes a serem executados."
-  echo "Exemplo: $0 10"
+  echo "Incorrect usage! Please provide the number of clients to be executed."
+  echo "Example: $0 10"
   exit 1
 fi
 
-# Número de clientes fornecido como argumento
-NUM_CLIENTES=$1
+# Number of clients
+NUM_CLIENTS=$1
 
-num_queues=$((NUM_CLIENTES / 2))
-num_publishers=$((NUM_CLIENTES / 2))
-num_consumers=$((NUM_CLIENTES / 2))
+num_queues=$((NUM_CLIENTS / 2))
+num_publishers=$((NUM_CLIENTS / 2))
+num_consumers=$((NUM_CLIENTS / 2))
 
-# Iniciar o servidor em um contêiner com base na imagem que você construiu
+# Start the server in a container based on the image builted
 docker run -d --name servidor -p 5672:5672 amqp
 
-# Aguarde alguns segundos para garantir que o servidor esteja totalmente iniciado
+# Wait for a few seconds to ensure the server is fully started
 sleep 10
 
-# Criar filas
+# Create queues
 for i in $(seq 1 $num_queues); do
   queue_name="queue_$i"
   amqp-declare-queue -q "$queue_name"
 done
 
-# Executar os publishers
+# Execute the publishers
 for i in $(seq 1 $num_publishers); do
   queue_name="queue_$i"
   message="message_$i"
   amqp-publish -r "$queue_name" -b "$message"
 done
 
-# Executar os consumers
+# Wait for a period for the publishers to finish sending messages
+sleep 10
+
+# Execute the consumers
 for i in $(seq 1 $num_consumers); do
   queue_name="queue_$i"
   amqp-consume -q "$queue_name" -c 5 cat &
 done
 
-# Aguarde a execução por um período de tempo (ajuste conforme necessário)
+# Wait for execution
 sleep 30
 
-# Coletar métricas de uso de rede e CPU do contêiner
+# Collect network and CPU usage metrics from the server container
 docker stats servidor --no-stream --format "table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}"
 
-# Parar e remover o contêiner do servidor
+# Stop and remove the server container
 docker stop servidor
 docker rm servidor
-
