cfbcacd57259a83d9e9451292443b57ad2cc771b
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-09 01:05:20 -0300

    Update tests.sh

--- 6th_Redes_MAC0352/EP1/scripts/tests.sh
+++ 6th_Redes_MAC0352/EP1/scripts/tests.sh
@@ -16,23 +16,29 @@ for NUM_CLIENTS in "${scenarios[@]}"; do
   output_file="results_${NUM_CLIENTS}_clients.txt"
   echo "docker stats:" > "$output_file"
 
-  # Loop para as operações alternadas
-  for i in $(seq 1 $num_queues); do
+  i=0 
+
+  for ((j = 0; j < 60; j++)); do
     queue_name="queue_$i"
     message="message_$i"
 
-    # Criação de fila
-    amqp-declare-queue -q "$queue_name"
+    # Verifique se i é menor que num_queues
+    if [ $i -lt $num_queues ]; then
+      # Criação de fila
+      amqp-declare-queue -q "$queue_name"
 
-    # Publicação de mensagem
-    amqp-publish -r "$queue_name" -b "$message"
+      # Publicação de mensagem
+      amqp-publish -r "$queue_name" -b "$message"
 
-    # Consumo de mensagem
-    amqp-consume -q "$queue_name" -c 5 cat &
+      # Consumo de mensagem
+      amqp-consume -q "$queue_name" -c 5 cat &
+
+      i=$((i + 1))
+    fi
 
     # Executa docker stats
     docker stats servidor --no-stream --format "table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}" >> "$output_file"
-    sleep 2
+    sleep 1
   done
 
   docker stop servidor
