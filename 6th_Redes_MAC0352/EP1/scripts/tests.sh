cc9d51b9137ac3d54b9a25f9ad605fb77e8cbb60
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-09 03:41:00 -0300

    Update tests.sh

--- 6th_Redes_MAC0352/EP1/scripts/tests.sh
+++ 6th_Redes_MAC0352/EP1/scripts/tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Array of scenarios (0, 10, and 100 clients)
 scenarios=(0 10 100)
 
 for NUM_CLIENTS in "${scenarios[@]}"; do
@@ -7,25 +8,32 @@ for NUM_CLIENTS in "${scenarios[@]}"; do
   num_publishers=$((NUM_CLIENTS / 2))
   num_consumers=$((NUM_CLIENTS / 2))
 
+  # Start the server in a container based on the image built
   docker run -d --name servidor -p 5672:5672 amqp
 
+  # Wait for a few seconds to ensure the server is fully started
   sleep 10
 
-  output_file="results_${NUM_CLIENTS}_clients.txt"
-  echo "docker stats:" > "$output_file"
+  # Create queues
+  for i in $(seq 1 $num_queues); do
+    queue_name="queue_$i"
+    amqp-declare-queue -q "$queue_name"
+  done
 
-  i=0 
-
-  for ((j = 0; j < 60; j++)); do
+  # Execute the publishers
+  for i in $(seq 1 $num_publishers); do
     queue_name="queue_$i"
     message="message_$i"
+    amqp-publish -r "$queue_name" -b "$message"
+  done
 
-    if [ $i -lt $num_queues ]; then
-      amqp-declare-queue -q "$queue_name"
-      amqp-publish -r "$queue_name" -b "$message"
-      amqp-consume -q "$queue_name" -c 5 cat &
-      i=$((i + 1))
-    fi
+  # Wait for a period for the publishers to finish sending messages
+  sleep 10
+
+  # Execute the consumers
+  for i in $(seq 1 $num_consumers); do
+    queue_name="queue_$i"
+    amqp-consume -q "$queue_name" -c 5 cat &
   done
 
   for ((j = 0; j < 60; j++)); do
@@ -40,8 +48,6 @@ for NUM_CLIENTS in "${scenarios[@]}"; do
 
   net_io_sum=$(awk -F'[/ ]+' '{ sum += $2; sum2 += $3 } END { print "Sum In: " sum " Sum Out: " sum2 }' "$output_file")
 
-
   echo "Média da CPUPerc: $cpu_average" >> "$output_file"
   echo "Soma dos bytes da NetIO: $net_io_sum" >> "$output_file"
-
 done
