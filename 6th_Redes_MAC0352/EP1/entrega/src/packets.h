62d7969c64979a311063a01de20e8778f343ce82
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-09 05:58:02 -0300

    entrega

---
+++ 6th_Redes_MAC0352/EP1/entrega/src/packets.h
@@ -0,0 +1,36 @@
+#ifndef PACKETS_H
+#define PACKETS_H
+
+#include <sys/types.h>
+
+extern const char PACKET_CONNECTION_START[];
+extern const size_t PACKET_CONNECTION_START_SIZE;
+
+extern const char PACKET_CONNECTION_TUNE[];
+extern const size_t PACKET_CONNECTION_TUNE_SIZE;
+
+extern const char PACKET_CONNECTION_OPEN_OK[];
+extern const size_t PACKET_CONNECTION_OPEN_OK_SIZE;
+
+extern const char PACKET_CONNECTION_CLOSE_OK[];
+extern const size_t PACKET_CONNECTION_CLOSE_OK_SIZE;
+
+extern const char PACKET_CHANNEL_OPEN_OK[];
+extern const size_t PACKET_CHANNEL_OPEN_OK_SIZE;
+
+extern const char PACKET_CHANNEL_CLOSE_OK[];
+extern const size_t PACKET_CHANNEL_CLOSE_OK_SIZE;
+
+extern const char PACKET_BASIC_CONSUME_OK[];
+extern const size_t PACKET_BASIC_CONSUME_OK_SIZE;
+
+extern const char PACKET_BASIC_DELIVER[];
+extern const size_t PACKET_BASIC_DELIVER_SIZE;
+
+extern const char PACKET_BASIC_QOS_OK[];
+extern const size_t PACKET_BASIC_QOS_OK_SIZE;
+
+void queuePacket(char *queueName, char *packet, int *packetSize, u_int32_t size);
+void consumePacket(char *queueName, char *packet, int *packetSize, char *message);
+
+#endif
