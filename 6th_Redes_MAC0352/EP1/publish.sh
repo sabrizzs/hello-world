e3132b21fa56335c4d1d8581af097a97e008ec61
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-09 05:39:30 -0300

    s

---
+++ 6th_Redes_MAC0352/EP1/publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+num_publishers=$1
+
+for i in $(seq 1 $num_publishers); do
+  queue_name="queue_$i"
+  message="message_$i"
+  amqp-publish -r "$queue_name" -b "$message"
+  sleep 1
+done
