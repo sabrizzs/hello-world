f22c1be31963c3c7f74cd8c89c37238e4fd1dafe
Sabrina Araújo <sabrinaaraujo@usp.br>
2023-11-03 22:36:29 -0300

    REDES

--- 6th_Redes_MAC0352/EP2/src/client.py
+++ 6th_Redes_MAC0352/EP2/src/client.py
@@ -1,1 +1,102 @@
-# client
+import socket
+import ssl
+import sys
+
+class Cliente:
+    def __init__(self, host, port):
+        self.IP = host
+        self.PORT = port
+        self.prompt = "> "
+        self.exit_manual = [False]
+    
+    def conexao(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.IP, self.PORT))
+        PORTA_SSL = int(s.recv(5).decode('utf-8'))
+
+        if PORTA_SSL == '':
+            print("Erro: Não foi possível obter a porta SSL do servidor.")
+            return
+
+        PORTA_SSL = int(PORTA_SSL)
+
+        socket_background = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_background.connect((self.IP, PORTA_SSL))
+
+        contexto = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        contexto.check_hostname = False
+        contexto.verify_mode = ssl.CERT_NONE
+
+        socket_ssl = contexto.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=self.IP)
+        socket_ssl.connect((self.IP, PORTA_SSL))
+
+        self.enviar_comando_socket('OK', socket_ssl)
+
+        thread_background = threading.Thread(target=self.servidor_background, args=(socket_background, s, socket_ssl, exit_manual))
+        thread_background.start()
+
+        while True:
+            out = ''
+            try:
+                while not out:
+                    out = input(self.prompt)
+            except:
+                self.exit_manual[0] = True
+                socket_ssl.close()
+                socket_background.close()
+                break
+
+            first = out.split()[0]
+
+            try:
+                if first == 'exemplo_comando':
+                    print("Recebeu o comando")
+
+            except Exception as e:
+                print("Terminando o programa")
+                break
+
+    def servidor_background(self, backsocket, ss):
+        while not self.manual_death[0]:
+            try:
+                data = backsocket.recv(1024)
+                if not data:
+                    break
+                print(data.decode('utf-8'))
+            except Exception as e:
+                break
+
+    def enviar_comando_socket(command: str, s: socket.socket):
+        command = bytearray(command.encode())
+        s.sendall(command)
+
+    def try_to_send_command(command: str, used_socket: socket.socket, s: socket.socket, backsocket: socket.socket, ss: ssl.SSLSocket):
+        send_command_to_socket(command, used_socket)
+        resp = receive_string_from_socket(used_socket)
+
+        while resp == '':
+            #Conexão fechada
+            success, s, backsocket, ss = update_sockets()
+            if not success:
+                break
+            send_command_to_socket(command, s)
+            resp = receive_string_from_socket(s)
+
+        return resp, s, backsocket, ss
+
+def main():
+    if len(sys.argv) == 3:
+        ip = sys.argv[1]
+        porta = int(sys.argv[2])
+    else:
+        print("Nenhum argumento fornecido. Forneça as portas como argumentos, por exemplo: python client.py 127.0.0.1 8080") 
+        #temporario
+        ip = '127.0.0.1'
+        porta = 8080
+        #exit(1)
+    
+    cliente = Cliente(ip, porta)
+    cliente.conexao()
+    
+if __name__ == "__main__":
+    main()
--- 6th_Redes_MAC0352/EP2/src/server.py
+++ 6th_Redes_MAC0352/EP2/src/server.py
@@ -1,95 +1,79 @@
-import os
-import signal
 import sys
+import socket
 
-#from tcp import ServidorTCP
-#from udp import ServidorUDP
-#from backend import Backend
+class Servidor:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def iniciar(self):
+        pass
+
+    def aceitar_conexao(self):
+        pass
+
+    def encerrar(self):
+        self.socket.close()
+
+class ServidorTCP(Servidor):
+    def iniciar(self):
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(5)
+        print(f"Servidor TCP rodando na porta {self.port}")
+
+        log = Log("servidor_log.txt")
+        usuarios_conectados = Usuarios()  # Crie uma instância de usuários
+
+    def aceitar_conexao(self):
+        conn, addr = self.socket.accept()
+        print(f"Conexão estabelecida com {addr}")
+        # Implemente o tratamento dessa conexão aqui
+
+class ServidorUDP(Servidor):
+    def iniciar(self):
+        self.socket.bind((self.host, self.port))
+        print(f"Servidor UDP rodando na porta {self.port}")
+
+    def receber_dados(self):
+        data, addr = self.socket.recvfrom(1024)
+        print(f"Dados recebidos de {addr}: {data.decode('utf-8')}")
+        # Implemente o tratamento dos dados UDP aqui
+
+class Log:
+    def __init__(self, arquivo):
+        self.arquivo = arquivo
+
+class Usuarios:
+    def __init__(self):
+        self.usuarios = {}
+
+    def criar_usuario(self, username, senha):
+        self.usuarios[username] = senha
+
+    def verificar_autenticacao(self, username, senha):
+        return username in self.usuarios and self.usuarios[username] == senha
+        
 def main():
-    # Verifica se foram passados dois argumentos na linha de comando
     if len(sys.argv) == 3:
         porta_tcp = int(sys.argv[1])
         porta_udp = int(sys.argv[2])
     else:
-        print("Nenhum argumento fornecido. Usando portas padrão para TCP e UDP.")
-        porta_tcp = 8080 
-        porta_udp = 12345 
+        print("Nenhum argumento fornecido. Forneça as portas como argumentos, por exemplo: python servidor.py 8080 12345") 
+        #temporario
+        porta_tcp = 8080
+        porta_udp = 12345
+        #exit(1)
 
-    print("Servidor TCP rodando na porta:", porta_tcp)
-    print("Servidor UDP rodando na porta:", porta_udp)
-
-    pid_tcp, pid_udp = None, None
-    '''
-    cria servidor
-    servidor = Backend()
-    '''
-
-
-    # Cria um processo filho para o servidor TCP
-    if (pid_tcp := os.fork()) == 0:
-        print("Processo filho do TCP criado")
-        '''
-        registra mensagens do TCP
-        common/logger.cp
-        logger_tcp = Logger("TCP", "tcp.txt")
-        '''
-
-        '''
-        if servidor.temUsuariosAtivos():
-            logger_tcp.escrever("Principal", "O servidor foi encerrado enquanto clientes estavam conectados! Reconectando!")
-        '''
-        
-
-        # Define um sinal para matar o processo pai se ele morrer
-        # prctl(PR_SET_PDEATHSIG, SIGKILL);
-
-        '''
-        common/util.cpp
-        resetCleanUpHook()
-        '''
-
-        '''
-        servidor_tcp = ServidorTCP(sys.argv[1])
-        servidor_tcp.escutarMensagens(servidor, logger_tcp)
-        '''
-        
-        sys.exit(0)
-
-    # Cria um processo filho para o servidor UDP
-    if (pid_udp := os.fork()) == 0:
-        print("Processo filho do UDP criado")
-
-        '''
-        registra mensagens do TCP
-        common/logger.cp
-        logger_tcp = Logger("UDP", "udp.txt")
-        '''
-
-        '''
-        if servidor.temUsuariosAtivos():
-            logger_tcp.escrever("Principal", "O servidor foi encerrado enquanto clientes estavam conectados! Reconectando!")
-        '''
-        
-
-        # Define um sinal para matar o processo pai se ele morrer
-        # os.prctl(os.PR_SET_PDEATHSIG, signal.SIGKILL)
-
-        '''
-        common/util.cpp
-        resetCleanUpHook()
-        '''
-        
-        '''
-        servidor_udp = ServidorUTP(sys.argv[1])
-        servidor_udp.escutarMensagens(servidor, logger_udp)
-        '''
-        
-        sys.exit(0)
-
-    # Aguarda a finalização dos processos filhos
-    os.waitpid(pid_tcp, 0)
-    os.waitpid(pid_udp, 0)
+    servidor_tcp = ServidorTCP('127.0.0.1', porta_tcp)
+    servidor_tcp.iniciar()
+    try:
+        while(True):
+            servidor_tcp.aceitar_conexao()
+    except:
+        print("Servidor finalizado")
+    
+    #servidor_udp = ServidorUDP('127.0.0.1', porta_udp)
 
 if __name__ == "__main__":
     main()
